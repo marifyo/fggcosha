7c2584bcb7ec80ef9adec94036c3b6861a17dc46
marifyo <effieobi@gmail.com>
2015-10-02 15:53:07 -0500

    t

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -73,7 +73,9 @@ $(document).ready(function() {
       navigation : true, // Show next and prev buttons
       slideSpeed : 300,
       paginationSpeed : 400,
-      singleItem:true
+      singleItem:true,
+      transitionStyle : "fade",
+      autoPlay: true
  
       // "singleItem:true" is a shortcut for:
       // items : 1, 
